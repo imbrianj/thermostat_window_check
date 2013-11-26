34b496364b895d10f1f60b24391cf0d278fe024f
imbrianj <bjohnsonwsu@gmail.com>
2013-11-25 20:52:01 -0800

    Now have option to turn thermostats off after a given time (so if you open the window briefly, it wont shut everything down)

--- thermostat_window_check.groovy
+++ thermostat_window_check.groovy
@@ -22,6 +22,14 @@ preferences {
     input "sendPushMessage", "enum", title: "Send a push notification?", metadata: [values: ["Yes", "No"]], required: false
     input "phone", "phone", title: "Send a Text Message?", required: false
   }
+
+  section("Turn thermostat off automatically?") {
+    input "turnOffTherm", "enum", metadata: [values: ["Yes", "No"]], required: false
+  }
+
+  section("Delay to wait before turning thermostat off (defaults to 1 minute)") {
+    input "turnOffDelay", "decimal", title: "Number of minutes", required: false
+  }
 }
 
 def installed() {
@@ -31,7 +39,6 @@ def installed() {
 
 def updated() {
   unsubscribe()
-  log.debug("Updated with settings: ${settings}")
   subscribe(thermostats, "thermostatMode", thermoChange);
   subscribe(sensors, "contact.open", windowChange);
 }
@@ -42,6 +49,8 @@ def thermoChange(evt) {
     def open = sensors.findAll { it?.latestValue("contact") == "open" }
 
     if(open) {
+      thermoShutOffTrigger()
+
       def plural = open.size() > 1 ? "are" : "is"
       send("${open.join(', ')} ${plural} still open and the thermostat just came on.")
     }
@@ -57,10 +66,36 @@ def windowChange(evt) {
   def cooling = thermostats.findAll { it?.latestValue("thermostatMode") == "cool" }
 
   if(heating || cooling) {
+    thermoShutOffTrigger()
+
     def open = sensors.findAll { it?.latestValue("contact") == "open" }
     def tempDirection = heating ? "heating" : "cooling"
     def plural = open.size() > 1 ? "were" : "was"
     send("${open.join(', ')} ${plural} opened and the thermostat is still ${tempDirection}.")
+  }
+}
+
+def thermoShutOffTrigger() {
+  if(turnOffTherm == "Yes") {
+    log.info("Starting timer to turn off thermostat")
+    def delay = (turnOffDelay != null && turnOffDelay != "") ? turnOffDelay * 60 : 60 
+    state.turnOffTime = now()
+
+    runIn(delay, "thermoShutOff")
+  }
+}
+
+def thermoShutOff() {
+  def open = sensors.findAll { it?.latestValue("contact") == "open" }
+  def tempDirection = heating ? "heating" : "cooling"
+  def plural = open.size() > 1 ? "are" : "is"
+
+  log.info("Checking if we need to turn thermostats off")
+
+  if(open.size()) {
+    send("Thermostats turned off: ${open.join(', ')} ${plural} open and thermostats ${tempDirection}.")
+    log.info("Windows still open, turning thermostats off")
+    thermostats?.off()
   }
 }
 
